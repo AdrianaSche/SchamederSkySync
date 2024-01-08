4a7686d5dfbaf85550c7018802de02953b955caa
Asce <87305735+AdrianaSche@users.noreply.github.com>
2024-01-08 20:30:45 +0100

    remove choice between custom tab and webbrowser

--- app/src/main/java/com/asce/schamederskysync/MainActivity.java
+++ app/src/main/java/com/asce/schamederskysync/MainActivity.java
@@ -1,12 +1,10 @@
 package com.asce.schamederskysync;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 import android.net.Uri;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.PopupMenu;
 import androidx.browser.customtabs.CustomTabsIntent;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,43 +19,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setupButtonWithLongClickListener(R.id.vereinsflieger, vereinsfliegerUrl);
-        setupButtonWithLongClickListener(R.id.btnWetter, wetterUrl);
-        setupButtonWithLongClickListener(R.id.btnFlightBook, flightBookUrl);
-        setupButtonWithLongClickListener(R.id.btnGliderTracker, gliderTrackerUrl);
+        setupButtonWithClickListener(R.id.vereinsflieger, vereinsfliegerUrl);
+        setupButtonWithClickListener(R.id.btnWetter, wetterUrl);
+        setupButtonWithClickListener(R.id.btnFlightBook, flightBookUrl);
+        setupButtonWithClickListener(R.id.btnGliderTracker, gliderTrackerUrl);
     }
 
-    private void setupButtonWithLongClickListener(int buttonId, String url) {
+    private void setupButtonWithClickListener(int buttonId, String url) {
         Button button = findViewById(buttonId);
-        button.setOnLongClickListener(view -> {
-            showPopupMenu(view, url);
-            return true;
+        button.setOnClickListener(item -> {
+            openCustomTab(url);
         });
-    }
-
-    private void showPopupMenu(View view, String url) {
-        PopupMenu popup = new PopupMenu(MainActivity.this, view);
-        popup.getMenuInflater().inflate(R.menu.open_link_menu, popup.getMenu());
-        popup.setOnMenuItemClickListener(item -> {
-            if(item.getItemId() == R.id.open_in_new_tab) {
-                openCustomTab(url);
-            }else {
-                openInBrowser(url);
-            }
-            return true;
-        });
-        popup.show();
     }
 
     private void openCustomTab(String url) {
         CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
         CustomTabsIntent customTabsIntent = builder.build();
-        customTabsIntent.intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        customTabsIntent.launchUrl(MainActivity.this, Uri.parse(url));
+
+        // try to open Custom Tabs
+        if (customTabsIntent.intent.resolveActivity(getPackageManager()) != null) {
+            customTabsIntent.intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            customTabsIntent.launchUrl(MainActivity.this, Uri.parse(url));
+        } else {
+            // Fallback: open Link in Webbrowser
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            browserIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(browserIntent);
+        }
     }
 
-    private void openInBrowser(String url) {
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-        startActivity(browserIntent);
-    }
+
 }
